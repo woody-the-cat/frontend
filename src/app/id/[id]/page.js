bb00ef16b1948085dac83ba5a612123930f5b93a
Deveroonie <74148830+Deveroonie@users.noreply.github.com>
2024-11-22 23:17:07 +0000

    you're links smell bad mate

--- src/app/id/[id]/page.js
+++ src/app/id/[id]/page.js
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 
 import Image from "next/image";
-import Link from 'next/link'
 
 export default function WoodyByID() {
     const params = useParams()
@@ -28,7 +27,7 @@ export default function WoodyByID() {
         return(
             <div>
                 <h1 className="text-white text-4xl font-bold">An error has occoured.</h1>
-                <p className="text-white">This is likely due to your search.<br /><Link href="/">Return to homepage.</Link></p>
+                <p className="text-white">This is likely due to your search.<br /><a href="/">Return to homepage.</a></p>
             </div>
         )
     } else {
@@ -39,10 +38,10 @@ export default function WoodyByID() {
                 <Image src={res.Image}  width={512} height={512} alt={"woody number " + id} title="woody" className="border border-white border-x-4 border-y-4"/>
                 <p className="text-white">
                   This Woody (ID): {id}<br />
-                  <Link href="/">Random Woody</Link>
+                  <a href="/">Random Woody</a>
                   <br /><br />
-                  <Link href={"https://purr.woody.cat/" + id}>Woody REST API (this Woody)</Link><br />
-                  <Link href="https://purr.woody.cat/">Woody REST API (random Woody)</Link>
+                  <a href={"https://purr.woody.cat/" + id}>Woody REST API (this Woody)</a><br />
+                  <a href="https://purr.woody.cat/">Woody REST API (random Woody)</a>
                 </p>
               </div>
             </div>
